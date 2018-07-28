655928d2dd4bcdbaa3a77a82856e16ce0d34f8ee
Terran <terrankroft@gmail.com>
2018-07-28 13:39:12 +1200

    Remove testing content dialogue

--- HelloWorld/MainPage.xaml.cs
+++ HelloWorld/MainPage.xaml.cs
@@ -848,15 +848,7 @@ namespace Protocol2
             Rect currentPosition = inkCanvas.InkPresenter.StrokeContainer.MoveSelected(new Point(0,0));
 
             inkCanvas.InkPresenter.StrokeContainer.MoveSelected(new Point(animation.startPoint.X  - (currentPosition.X + currentPosition.Width/2), animation.startPoint.Y - (currentPosition.Y + currentPosition.Height / 2)));
-            ContentDialog deleteFileDialog = new ContentDialog
-            {
-                Title = "Delete file permanently?",
-                Content = "If you delete this file, you won't be able to recover it. Do you want to delete it?",
-                PrimaryButtonText = "Delete",
-                CloseButtonText = "Cancel"
-            };
-
-            ContentDialogResult result = await deleteFileDialog.ShowAsync();
+    
             // want something here so we reset the location of ink to where it should start from
             // MoveStroke doesn't move it to a position relative to the canvas but rather relative to its current location!
 
